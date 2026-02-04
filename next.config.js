2338980f4ab967d3db7c58061f8fa4572ff809d1
unknown <tkqlek20@gmail.com>
2026-02-04 16:07:53 +0800

    移除 standalone 配置

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   images: {
     remotePatterns: [
@@ -9,18 +8,6 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-        ],
-      },
-    ];
   },
 };
 
